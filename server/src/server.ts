791834487b75ba283f507c1e8d6ef8c6884d3754
Paulo Daniel Dorensbach <dorensbach@gmail.com>
2022-05-11 22:46:31 -0300

    Rearranjo dos diretórios. Criação do diretório server

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+//req = request, res = response
+app.get('/users', (req, res) => {
+    return res.send('Hello World!');
+});
+
+app.listen(3333, () => {
+    console.log('HTTP server running');
+})
